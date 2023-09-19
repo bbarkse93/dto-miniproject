8738eeb787c7b37468c56e4f13c8eaa715abf6b8
saki8661 <saki8661@naver.com>
2023-09-19 20:10:15 +0900

    수정본

---
+++ src/test/java/com/example/kakao/order/OrderJPARepository.java
@@ -0,0 +1,14 @@
+package com.example.kakao.order;
+
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class OrderJPARepository {
+
+    @Test
+    public void findByid_test() {
+
+    }
+
+}
