4e8cdc4d3d13baa1edbfaf46b309caeb8547f952
GodBongJune <135561298+GodBongJune@users.noreply.github.com>
2023-09-21 12:39:44 +0900

    제품상세보기 완료
    [최봉준]
    
    제품상세보기 v1, v2완료

--- src/main/java/com/example/kakao/product/ProductResponse.java
+++ src/main/java/com/example/kakao/product/ProductResponse.java
@@ -1,5 +1,11 @@
 package com.example.kakao.product;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.kakao.product.ProductResponse.FindByIdV1DTO.OptionDTO;
+import com.example.kakao.product.option.Option;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,9 +21,85 @@ public class ProductResponse {
     }
 
     // (기능2) 상품 상세보기
+
     @Getter
     @Setter
     public static class FindByIdDTO {
+
+    }
+
+    // 상품조회+옵션조회
+    @Getter
+    @Setter
+    public static class FindByIdV1DTO {
+        private Integer productId;
+        private String productName;
+        private String productImage;
+        private Integer productPrice;
+        private Integer productStarCount;
+        private List<OptionDTO> options;
+
+        public FindByIdV1DTO(Product product, List<Option> options) {
+            this.productId = product.getId();
+            this.productName = product.getProductName();
+            this.productImage = product.getImage();
+            this.productPrice = product.getPrice();
+            this.productStarCount = 4;
+            this.options = product.getOptions().stream()
+                    .map(o -> new OptionDTO(o))
+                    .collect(Collectors.toList());
+        }
+
+        @Getter
+        @Setter
+        public class OptionDTO {
+            private Integer optionId;
+            private String optionName;
+            private Integer optionPrice;
+
+            public OptionDTO(Option option) {
+                this.optionId = option.getId();
+                this.optionName = option.getOptionName();
+                this.optionPrice = option.getPrice();
+            }
+        }
+    }
+
+    // 양방향맵핑
+    @Getter
+    @Setter
+    public static class FindByIdV2DTO {
+        private Integer productId;
+        private String productName;
+        private String productImage;
+        private Integer productPrice;
+        private Integer productStarCount;
+        private List<OptionDTO> options;
+
+        public FindByIdV2DTO(Product product) {
+            this.productId = product.getId();
+            this.productName = product.getProductName();
+            this.productImage = product.getImage();
+            this.productPrice = product.getPrice();
+            this.productStarCount = 4;
+            this.options = product.getOptions().stream()
+                    .map(o -> new OptionDTO(o))
+                    .collect(Collectors.toList());
+        }
+
+        @Getter
+        @Setter
+        public class OptionDTO {
+            private Integer optionId;
+            private String optionName;
+            private Integer optionPrice;
+
+            public OptionDTO(Option option) {
+                this.optionId = option.getId();
+                this.optionName = option.getOptionName();
+                this.optionPrice = option.getPrice();
+            }
+        }
 
     }
 }
--- src/main/java/com/example/kakao/product/ProductService.java
+++ src/main/java/com/example/kakao/product/ProductService.java
@@ -5,6 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.kakao._core.errors.exception.Exception404;
+import com.example.kakao.product.ProductResponse.FindByIdV1DTO;
+import com.example.kakao.product.option.Option;
+import com.example.kakao.product.option.OptionJPARepository;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -13,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class ProductService {
 
     private final ProductJPARepository productJPARepository;
+    private final OptionJPARepository optionJPARepository;
 
     // (기능1) 상품 목록보기
     public List<ProductResponse.FindAllDTO> findAll(int page) {
@@ -20,7 +26,18 @@ public class ProductService {
     }
 
     // (기능2) 상품 상세보기
-    public ProductResponse.FindByIdDTO findById(int id) {
-        return null;
+    // 상품조회 + 옵션 조회
+    public ProductResponse.FindByIdV1DTO findByIdV1(int id) {
+        Product productPS = productJPARepository.findById(id)
+                .orElseThrow(() -> new Exception404("해당 id의 상품을 찾을 수 없습니다 : " + id));
+        List<Option> optionsPS = optionJPARepository.findByProductId(id);
+        return new ProductResponse.FindByIdV1DTO(productPS, optionsPS);
+    }
+
+    // 양방향맵핑
+    public ProductResponse.FindByIdV2DTO findByIdV2(int id) {
+        Product productPS = productJPARepository.findById(id)
+                .orElseThrow(() -> new Exception404("해당 id의 상품을 찾을 수 없습니다 : " + id));
+        return new ProductResponse.FindByIdV2DTO(productPS);
     }
 }
