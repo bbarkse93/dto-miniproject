1229085ee761b193d05f3b090541f783aaa8ae34
wanni30 <hubhy@nate.com>
2023-09-29 23:53:56 +0900

    topic/AOP update V2

--- src/main/java/com/example/kakao/user/UserRequest.java
+++ src/main/java/com/example/kakao/user/UserRequest.java
@@ -5,7 +5,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-import org.hibernate.validator.constraints.UniqueElements;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,13 +16,15 @@ public class UserRequest {
     @ToString
     public static class JoinDTO {
 
+        @NotEmpty
         @Email(message = "유효한 이메일 주소를 입력하세요.")
-        @Column(unique = true) // 중복된 이메일 주소를 막기 위한 설정
         private String email;
 
         @NotEmpty
         @Size(min = 8, max = 20, message = "8에서 20자 이내여야 합니다.")
         private String password;
+
+        @NotEmpty
         private String username;
 
         public User toEntity() {
